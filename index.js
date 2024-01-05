34b062ebaabbf75eea4a3795e2ba2ed96f2e0dc4
Nihat Mammadli <nihatmammadli42@gmail.com>
2024-01-05 17:58:54 +0400

    three boxes and animation on it

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const arrow = document.querySelector(".arrow");
 
 arrow.addEventListener('click', e=> {
   window.scrollTo({
-    top:900,  
+    top:960,  
     behavior:"smooth",  
     
   });
@@ -10,10 +10,10 @@ arrow.addEventListener('click', e=> {
 })
 
 
-function on() {
-  document.querySelector(".overlay").style.display = "block";
-}
+// function on() {
+//   document.querySelector(".overlay").style.display = "block";
+// }
 
-function off() {
-  document.querySelector(".overlay").style.display = "none";
-}
+// function off() {
+//   document.querySelector(".overlay").style.display = "none";
+// }
