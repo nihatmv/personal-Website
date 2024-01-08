b8c97f0805873b515e16210173f6e8979bfd8a2b
Nihat Mammadli <nihatmammadli42@gmail.com>
2024-01-08 22:26:17 +0400

    social media part

--- index.js
+++ index.js
@@ -29,14 +29,3 @@ projects.addEventListener('click', e=> {
   });
 
 })
-
-
-
-
-// function on() {
-//   document.querySelector(".overlay").style.display = "block";
-// }
-
-// function off() {
-//   document.querySelector(".overlay").style.display = "none";
-// }
