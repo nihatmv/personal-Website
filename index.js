2e5a08ec23de782efce12bebcfd31a3ffdc2db91
Nihat Mammadli <nihatmammadli42@gmail.com>
2024-02-10 16:39:41 +0400

    final version

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
 const arrow = document.querySelector(".arrow");
-const aboutMe = document.querySelector(".about-me");
-const projects = document.querySelector(".projects");
+// const aboutMe = document.querySelector(".about-me");
+// const projects = document.querySelector(".projects");
+const aboutMe = document.querySelector(".about-me-script");
+const projects = document.querySelector(".projects-script");
+const socialMedia = document.querySelector(".social-media-script");
+const leftNavbar = document.querySelector(".left-navbar");
 
 aboutMe.addEventListener("click", (e) => {
   window.scrollTo({
-    top: 1960,
+    top: 360,
     behavior: "smooth",
   });
 });
 
 projects.addEventListener("click", (e) => {
   window.scrollTo({
-    top: 2860,
+    top: 1090,
+    behavior: "smooth",
+  });
+});
+
+socialMedia.addEventListener("click", (e) => {
+  window.scrollTo({
+    top: 5260,
+    behavior: "smooth",
+  });
+});
+leftNavbar.addEventListener("click", (e) => {
+  window.scrollTo({
+    top: 0,
     behavior: "smooth",
   });
 });
